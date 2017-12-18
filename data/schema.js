4b502c78fb58aa0c488720750040780e4835535a
Johanna Kate <johanna@ripple.com>
2017-12-18 14:58:18 -0800

    up cache age

--- data/schema.js
+++ data/schema.js
@@ -19,7 +19,7 @@ type Post {
 type Query {
   author(firstName: String, lastName: String): Author
   allAuthors: [Author]
-  getFortuneCookie: String @cacheControl(maxAge: 240)
+  getFortuneCookie: String @cacheControl(maxAge: 5000)
 }
 `;
 
